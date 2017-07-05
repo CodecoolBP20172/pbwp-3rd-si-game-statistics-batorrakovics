893e1469e1b9d81c3424ceff33a20335eb8c17c2
batorrakovics <rakovics.bator@gmail.com>
2017-07-05 12:31:52 +0200

    akhassfhsdgs

--- printing.py
+++ printing.py
@@ -3,4 +3,5 @@ import reports
 #Game count:
 count = reports.count_games("game_stat.txt")
 print(count)
-#
+#Is there a game from a specific year?:
+
--- reports.py
+++ reports.py
@@ -25,19 +25,29 @@ def get_latest(file_name):
         years.append(sub_list[2])
     maxyears = max(years)
     return (games_list[years.index(maxyears)])
-    
-        
-        
+
 def count_by_genre(file_name, genre):
     games_list = file_read(file_name)
     genre_list = []
+    genre_num = 0
     for sub_list in games_list:
         genre_list.append(sub_list[3])
+    for g in genre_list:
+        if g == genre:
+            genre_num += 1
+    return genre_num
 
 def get_line_number_by_title(file_name, title):
-    pass
+    game_list = file_read(file_name)
+    for i in range(len(game_list)):
+        if title == game_list[i][0]:
+            return i+1
+    raise ValueError
+            
 
 #file_read("game_stat.txt")
 #count_games("game_stat.txt")
 #decide("game_stat.txt", "2009")
-get_latest("game_stat.txt")
+#get_latest("game_stat.txt")
+#count_by_genre("game_stat.txt", "RPG")
+#get_line_number_by_title("game_stat.txt", "Minecraft")
